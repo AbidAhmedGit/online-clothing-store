ae386a06491a795cbe50b1e9cc4b15e911ba5301
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-07-17 11:42:59 -0400

    react router added with navigation bar on top

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import './navigation.styles.scss'
+
+const Navigation = () => {
+    return (
+      <>
+        <div className="navigation">
+            <Link className="logo-container" to="/">
+                <CrwnLogo />
+            </Link>
+            <div className = 'nav-links-container'>
+                <Link className='nav-link' to="/shop">
+                    SHOP
+                </Link>
+            </div>
+        </div>
+        <Outlet />
+      </>
+    )
+}
+
+export default Navigation
